c437479ca36eb5b9d968bb0f2c583da858c4e89b
Jeff <sjeffshah@gmail.com>
2019-11-24 15:22:11 -0500

    start python learning

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,7 @@
-print("Hello world, this is my first python file")
+print("Hello world, this is my first python file") 
+
+#In Python 2, the "print" statement is not a function, and therefore it is 
+# invoked without parentheses. However, in Python 3, it is a function, and must be invoked with parentheses.
+
+print("I am back, this time I will advance in Python")
+
